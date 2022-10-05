af30bd2510f2ebe123b08dba7e559199de298427
41k4 <98024900+41k4@users.noreply.github.com>
2022-10-05 11:17:35 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,14 @@
+function EuklidesOblicz(){
+    let liczbaA=parseInt(document.getElementById("liczbaA").value);
+    let liczbaB=parseInt(document.getElementById("liczbaB").value);
+    while(liczbaA!==liczbaB){
+        if(liczbaA>liczbaB){
+            liczbaA=liczbaA-liczbaB;
+        }else{
+            liczbaB=liczbaB-liczbaA;
+        }
+
+    }
+    document.getElementById("EuklidesWynik").value=liczbaA;
+
+}
